2947361a5c77a984e9fde093be496f267a03bd40
emmameregalli <emma.meregalli03@gmail.com>
2025-05-29 13:45:40 +0200

    Update

--- src/PolyhedralTriangulation.cpp
+++ src/PolyhedralTriangulation.cpp
@@ -10,6 +10,34 @@ using namespace Eigen;
 
 namespace PolyhedralLibrary {
 
+    vector<int> ComputeVEF(unsigned int q, int b, int c)
+    {
+        vector<int> VEF(3, 0);  // inizializzo un vettore nullo di lunghezza 3
+
+        unsigned int T = b * b + b * c + c * c;
+        unsigned int V, E, F;
+
+        if (q == 3) {
+            V = 2 * T + 2;
+            E = 6 * T;
+            F = 4 * T;
+        } else if (q == 4) {
+            V = 4 * T + 2;
+            E = 12 * T;
+            F = 8 * T;
+        } else {
+            V = 10 * T + 2;
+            E = 30 * T;
+            F = 20 * T;
+        }
+
+        VEF[0] = V;  // V = vertici
+        VEF[1] = E;  // E = spigoli
+        VEF[2] = F;  // F = facce
+
+        return VEF;  // Mi viene restituito il vettore con i valori di V, E, F
+    }
+
     // Funzione ausiliaria per aggiungere un lato alla mesh triangolata, evitando duplicati
     void AddOrFindEdge(
         int vStart, int vEnd, // estremi del lato
@@ -77,7 +105,7 @@ namespace PolyhedralLibrary {
 
         // Allocazione memoria per vertici (0D)
         triMesh.Cell0DsId.resize(duplicatedDimensions[0]);
-        triMesh.Cell0DsCoordinates = MatrixXd::Zero(3, duplicatedDimensions[0]);
+        triMesh.Cell0DsCoordinates = MatrixXd::Zero(duplicatedDimensions[0],3);
         triMesh.Cell0DsFlag.resize(duplicatedDimensions[0]);
 
         // Allocazione memoria per lati (1D)
@@ -87,8 +115,8 @@ namespace PolyhedralLibrary {
 
         // Allocazione memoria per facce (2D)
         triMesh.Cell2DsId.resize(duplicatedDimensions[2]);
-        triMesh.Cell2DsVertices.resize(duplicatedDimensions[2]);
-        triMesh.Cell2DsEdges.resize(duplicatedDimensions[2]);
+        triMesh.Cell2DsVertices.resize(duplicatedDimensions[2],3);
+        triMesh.Cell2DsEdges.resize(duplicatedDimensions[2],3);
 
         // Contatori per vertici, lati e facce
         unsigned int vCount = 0, eCount = 0, fCount = 0;
